bdadb7e91d06d877718fd4ca7a9fcce593636a46
pd4d10 <pd4d10@gmail.com>
2016-11-22 16:17:22 +0800

    Add comment, and add some rules

--- index.js
+++ index.js
@@ -29,31 +29,70 @@ module.exports = {
     jquery: true,
   },
   rules: {
-    indent: ['error', 4, { SwitchCase: 1 }], // 缩进
-    'max-len': ['error', { code: 240, tabWidth: 4, ignoreUrls: true, ignoreComments: false }],
-    camelcase: 'off', // 不强制使用 camel case 命名
-    'consistent-return': 'off', // 不强制 return
-    'global-require': 'off', // 不强制全局 require
-    'newline-per-chained-call': 'off', // 不强制链接操作另起一行
-    'no-underscore-dangle': 'off', // 不禁止使用下划线作为变量名前缀
-    'comma-dangle': 'off', // 不强制尾逗号
-    'no-console': 'off', // 不禁用 console
-    'prefer-arrow-callback': 'off', // 不强制使用箭头函数
-    'import/no-amd': 'off', // 不禁用 amd
-    'func-names': 'off', // 不强制函数表达式有函数名
+    // 缩进
+    indent: ['error', 4, { SwitchCase: 1 }],
+
+    // 代码长度
+    'max-len': ['error', {
+      code: 240,
+      tabWidth: 4,
+      ignoreUrls: true,
+      ignoreComments: false,
+    }],
+
+    // 不强制使用 camel case 命名
+    camelcase: 'off',
+
+    // 不强制 return
+    'consistent-return': 'off',
+
+    // 不强制全局 require
+    'global-require': 'off',
+
+    // 不强制链接操作另起一行
+    'newline-per-chained-call': 'off',
+
+    // 不强制尾逗号
+    'no-underscore-dangle': 'off',
+    'comma-dangle': 'off',
+
+    // 不禁用 console
+    'no-console': 'off',
+
+    // 不禁用 console
+    'prefer-arrow-callback': 'off',
+
+    // 不禁用 amd
+    'import/no-amd': 'off',
+
+    // 不强制函数表达式有函数名
+    'func-names': 'off',
+
+    // for in 不强制检查 hasOwnProperty
     'guard-for-in': 'off',
+
+    // 不禁止函数参数重新赋值
     'no-param-reassign': 'off',
-    'no-restricted-syntax': [2, 'DebuggerStatement', 'LabeledStatement', 'WithStatement'], // 不禁止 for in 循环
-    'arrow-parens': 'off',
-    'padded-blocks': 'off',
+
+    // 不禁止 for in 循环
+    'no-restricted-syntax': [2, 'DebuggerStatement', 'LabeledStatement', 'WithStatement'],
+
+    // 不禁止 ++ 操作符
     'no-plusplus': 'off',
-    'no-mixed-operators': 'off',
-    'no-danger-with-children': 'off',
-    'padded-blocks': 'off',
+
+    // 不强制 class 方法使用 this
     'class-methods-use-this': 'off',
-    'import/newline-after-import': 'off',
+
+    // 不禁止 require 变量
     'import/no-dynamic-require': 'off',
-    'import/no-extraneous-dependencies': 'off',
+
+    // 不禁止 require devDependencies 中的包
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true,
+      optionalDependencies: false,
+    }],
+
+    // 不强制 export default（只 export 一次）
     'import/prefer-default-export': 'off',
-  }
-}
+  },
+};
